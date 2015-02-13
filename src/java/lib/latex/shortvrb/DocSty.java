0ee210f4f138c8f2bf8ce54ac984a017b975b37a
nlct <n.talbot@uea.ac.uk>
2015-02-13 20:02:35 +0000

    Started DocSty

---
+++ src/java/lib/latex/shortvrb/DocSty.java
@@ -0,0 +1,51 @@
+/*
+    Copyright (C) 2013 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texparserlib.latex.shortvrb;
+
+import java.io.IOException;
+
+import com.dickimawbooks.texparserlib.*;
+import com.dickimawbooks.texparserlib.latex.*;
+
+public class DocSty extends LaTeXSty
+{
+   public DocSty(LaTeXParserListener listener)
+   {
+      super("doc", listener);
+   }
+
+   public void addDefinitions()
+   {
+      registerControlSequence(
+         new StoreDataCs("IndexPrologue", "index@prologue"));
+
+   }
+
+   public void processOption(String option)
+    throws IOException
+   {
+   }
+
+   protected void preOptions()
+     throws IOException
+   {
+      ((LaTeXParserListener)getListener()).usepackage(null, "shortvrb");
+   }
+
+}
