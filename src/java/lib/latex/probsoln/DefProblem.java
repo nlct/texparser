3ccdcffb6cdea6bf167bbac96534521ecc0c74d4
nlct <n.talbot@uea.ac.uk>
2015-04-15 19:22:21 +0100

    Started on implementing probsoln

---
+++ src/java/lib/latex/probsoln/DefProblem.java
@@ -0,0 +1,218 @@
+/*
+    Copyright (C) 2013 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texparserlib.latex.probsoln;
+
+import java.io.IOException;
+import java.io.EOFException;
+
+import com.dickimawbooks.texparserlib.*;
+import com.dickimawbooks.texparserlib.latex.*;
+
+public class DefProblem extends Declaration
+{
+   public DefProblem(ProbSolnSty sty)
+   {
+      this("defproblem", sty);
+   }
+
+   public DefProblem(String name, ProbSolnSty sty)
+   {
+      super(name);
+      this.sty = sty;
+   }
+
+   public Object clone()
+   {
+      return new DefProblem(getName(), sty);
+   }
+
+   public TeXObjectList expandonce(TeXParser parser, TeXObjectList list)
+      throws IOException
+   {
+      return null;
+   }
+
+   public TeXObjectList expandonce(TeXParser parser)
+      throws IOException
+   {
+      return null;
+   }
+
+   public TeXObjectList expandfully(TeXParser parser, TeXObjectList list)
+      throws IOException
+   {
+      return null;
+   }
+
+   public TeXObjectList expandfully(TeXParser parser)
+      throws IOException
+   {
+      return null;
+   }
+
+   public void process(TeXParser parser) throws IOException
+   {
+      process(parser, parser);
+   }
+
+   public void process(TeXParser parser, TeXObjectList stack) throws IOException
+   {
+      Numerical numArgs = null;
+      TeXObjectList defArgs = null;
+
+      String label;
+
+      if (parser == stack)
+      {
+         numArgs = parser.popNumericalArg('[', ']');
+
+         if (numArgs != null)
+         {
+            TeXObject obj = parser.popNextArg('[', ']');
+
+            if (obj instanceof TeXObjectList)
+            {
+               defArgs = (TeXObjectList)obj;
+            }
+            else if (obj != null)
+            {
+               defArgs = new TeXObjectList();
+               defArgs.add(obj);
+            }
+         }
+
+         TeXObject labObj = parser.popNextArg();
+
+         TeXObjectList expanded = null;
+
+         if (labObj instanceof Expandable)
+         {
+            expanded = ((Expandable)labObj).expandfully(parser);
+         }
+
+         if (expanded == null)
+         {
+            label = labObj.toString(parser);
+         }
+         else
+         {
+            label = expanded.toString(parser);
+         }
+
+         // ignore final optional argument
+         parser.popNextArg('[', ']');
+      }
+      else
+      {
+         numArgs = stack.popNumericalArg(parser, '[', ']');
+
+         if (numArgs != null)
+         {
+            TeXObject obj = stack.popArg(parser, '[', ']');
+
+            if (obj instanceof TeXObjectList)
+            {
+               defArgs = (TeXObjectList)obj;
+            }
+            else if (obj != null)
+            {
+               defArgs = new TeXObjectList();
+               defArgs.add(obj);
+            }
+         }
+
+         TeXObject labObj = stack.popArg(parser);
+
+         TeXObjectList expanded = null;
+
+         if (labObj instanceof Expandable)
+         {
+            expanded = ((Expandable)labObj).expandfully(parser, stack);
+         }
+
+         if (expanded == null)
+         {
+            label = labObj.toString(parser);
+         }
+         else
+         {
+            label = expanded.toString(parser);
+         }
+
+         // ignore final optional argument
+         stack.popArg(parser, '[', ']');
+      }
+
+      TeXObject object = stack.pop();
+
+      TeXObjectList contents = new TeXObjectList();
+
+      while (object != null)
+      {
+         if (object instanceof TeXCsRef)
+         {
+            object = parser.getListener().getControlSequence(
+              ((TeXCsRef)object).getName());
+         }
+
+         if (object instanceof End)
+         {
+            TeXObject nextObj = 
+               stack == parser ? parser.popNextArg() : stack.popArg(parser);
+
+            String envName = envName = nextObj.toString(parser);
+
+            Group grp = parser.getListener().createGroup(envName);
+
+            if (envName.equals(getName()))
+            {
+               stack.push(grp);
+               stack.push(object);
+               break;
+            }
+
+            contents.add(object);
+            contents.add(grp);
+         }
+         else
+         {
+            contents.add(object);
+         }
+
+         object = stack.pop();
+      }
+
+      ProbSolnData data = new ProbSolnData(label, numArgs.number(parser),
+        defArgs, contents);
+
+      sty.addProblem(data);
+   }
+
+   public void end(TeXParser parser)
+    throws IOException
+   {
+   }
+
+   public boolean isModeSwitcher()
+   {
+      return false;
+   }
+
+   private ProbSolnSty sty;
+}
---
+++ src/java/lib/latex/probsoln/ProbSolnSty.java
@@ -0,0 +1,152 @@
+/*
+    Copyright (C) 2013 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texparserlib.latex.probsoln;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.dickimawbooks.texparserlib.*;
+import com.dickimawbooks.texparserlib.latex.*;
+
+public class ProbSolnSty extends LaTeXSty
+{
+   public ProbSolnSty(LaTeXParserListener listener)
+   {
+      super("probsoln", listener);
+
+      databases = new HashMap<String,ProbSolnDatabase>();
+
+      try
+      {
+         addDatabase("default");
+      }
+      catch (ProbSolnException e)
+      {
+         // won't happen
+      }
+   }
+
+   public void addDefinitions()
+   {
+      registerControlSequence(new DefProblem(this));
+   }
+
+   public void processOption(String option)
+    throws IOException
+   {
+   }
+
+   protected void preOptions()
+     throws IOException
+   {
+   }
+
+   public ProbSolnDatabase getDatabase(String name) throws ProbSolnException
+   {
+      ProbSolnDatabase db = databases.get(name);
+
+      if (db == null)
+      {
+         throw new ProbSolnException(
+           "ProbSoln Database '"+name+"' doesn't exist");
+      }
+
+      return db;
+   }
+
+   public ProbSolnData getProblem(String label, String dbName)
+    throws ProbSolnException
+   {
+      ProbSolnData prob = getDatabase(dbName).get(label);
+
+      if (prob == null)
+      {
+         throw new ProbSolnException(String.format(
+           "Entry '%s' doesn't exist in ProbSoln database '%s'",
+           label, dbName));
+      }
+
+      return prob;
+   }
+
+   public void addDatabase(String name) throws ProbSolnException
+   {
+      if (databases.containsKey(name))
+      {
+         throw new ProbSolnException(
+           "ProbSoln Database '"+name+"' already exists");
+      }
+
+      ProbSolnDatabase db = new ProbSolnDatabase(name);
+      databases.put(name, db);
+   }
+
+   public void moveProblem(String label, String source, String target)
+    throws ProbSolnException
+   {
+      ProbSolnDatabase db = databases.get(source);
+
+      if (db == null)
+      {
+         throw new ProbSolnException(
+           "ProbSoln Database '"+source+"' doesn't exist");
+      }
+
+      ProbSolnData data = db.remove(label);
+
+      if (data == null)
+      {
+         throw new ProbSolnException(String.format(
+           "Entry '%s' doesn't exist in ProbSoln database '%s'",
+           label, source));
+      }
+
+      db = databases.get(target);
+
+      if (db == null)
+      {
+         throw new ProbSolnException(
+           "ProbSoln Database '"+target+"' doesn't exist");
+      }
+
+      db.put(label, data);
+   }
+
+   public Set<String> getDatabaseLabels()
+   {
+      return databases.keySet();
+   }
+
+   public int getDatabaseCount()
+   {
+      return databases.size();
+   }
+
+   public void addProblem(ProbSolnData data) throws ProbSolnException
+   {
+      ProbSolnDatabase db = getDatabase(currentDb);
+
+      db.put(data.getName(), data);
+   }
+
+   private String currentDb = "default";
+
+   private HashMap<String,ProbSolnDatabase> databases;
+}
