e3d520f0a9ef3000da2c0d6d1af76011c04b5c50
nlct <n.talbot@uea.ac.uk>
2017-02-02 10:20:35 +0000

    Added more units

--- src/java/lib/latex/siunitx/SIunitxSty.java
+++ src/java/lib/latex/siunitx/SIunitxSty.java
@@ -24,6 +24,14 @@ import com.dickimawbooks.texparserlib.*;
 import com.dickimawbooks.texparserlib.latex.*;
 import com.dickimawbooks.texparserlib.primitives.Undefined;
 
+/*
+ * This is a very primitive implementation if siunitx. The main
+ * purpose is to help bib2gls interpret 'name' fields containing 
+ * scientific units when the 'sort' field is missing. This class is
+ * not intended as a fully-functional implementation of a large and
+ * complex package.
+ */
+
 public class SIunitxSty extends LaTeXSty
 {
    public SIunitxSty(KeyValList options, LaTeXParserListener listener)
@@ -52,7 +60,7 @@ public class SIunitxSty extends LaTeXSty
    {
       if (arg instanceof ControlSequence)
       {
-         TeXObject unit = getUnit(parser, (ControlSequence)arg);
+         TeXObject unit = getUnit((ControlSequence)arg);
 
          return unit == null ? arg : unit;
       }
@@ -155,7 +163,7 @@ public class SIunitxSty extends LaTeXSty
       }
       else
       {
-         TeXObject unit = getUnit(parser, cs);
+         TeXObject unit = getUnit(cs);
 
          return unit == null ? cs : unit;
       }
@@ -261,43 +269,233 @@ public class SIunitxSty extends LaTeXSty
       return true;
    }
 
-   public TeXObject getUnit(TeXParser parser, ControlSequence cs)
+   public TeXObject getUnit(ControlSequence cs)
    {
       String name = cs.getName();
 
-      for (int i = 0; i < UNITS.length; i++)
+      String val = getUnit(name, BASE_UNITS);
+
+      if (val != null)
       {
-         if (UNITS[i][0].equals(name))
-         {
-            return new SIUnitCs(this, name, UNITS[i][1]);
-         }
+         return new SIUnitCs(this, name, val);
       }
 
-      for (int i = 0; i < PREFIXES.length; i++)
+      val = getUnit(name, PREFIXES);
+
+      if (val != null)
       {
-         if (PREFIXES[i][0].equals(name))
+         return new SIPrefixCs(this, name, val);
+      }
+
+      val = getUnit(name, BINARY_PREFIXES);
+
+      if (val != null)
+      {
+         return new SIPrefixCs(this, name, val);
+      }
+
+      val = getUnit(name, DERIVED_UNITS);
+
+      if (val != null)
+      {
+         return new SIUnitCs(this, name, val);
+      }
+
+      val = getUnit(name, NON_SI_UNITS);
+
+      if (val != null)
+      {
+         return new SIUnitCs(this, name, val);
+      }
+
+      val = getUnit(name, ABBREVIATED_UNITS);
+
+      if (val != null)
+      {
+         return new SIUnitCs(this, name, val);
+      }
+
+      return null;
+   }
+
+   public String getUnit(String name, String[][] array)
+   {
+      for (int i = 0; i < array.length; i++)
+      {
+         if (array[i][0].equals(name))
          {
-            return new SIPrefixCs(this, name, PREFIXES[i][1]);
+            return array[i][1];
          }
       }
 
       return null;
    }
 
-   private static final String[][] UNITS = new String[][]
+   private static final String[][] BASE_UNITS = new String[][]
    {
       new String[]{"ampere", "A"},
       new String[]{"candela", "cd"},
-      new String[]{"farad", "F"},
-      new String[]{"gram", "g"},
-      new String[]{"gray", "Gy"},
-      new String[]{"kelvin", "K"},
+      new String[]{"kelvin", ""+(char)0x212A},
       new String[]{"kilogram", "kg"},
-      new String[]{"lux", "lx"},
+      new String[]{"gram", "g"},
       new String[]{"metre", "m"},
+      new String[]{"meter", "m"},
       new String[]{"mole", "mol"},
       new String[]{"second", "s"},
+   };
+
+   private static final String[][] DERIVED_UNITS = new String[][]
+   {
+      new String[]{"becquerel", "Bq"},
+      new String[]{"degreeCelsius", ""+(char)0x2103},
+      new String[]{"coulomb", "C"},
+      new String[]{"farad", "F"},
+      new String[]{"gray", "Gy"},
+      new String[]{"hertz", "Hz"},
+      new String[]{"henry", "H"},
+      new String[]{"joule", "J"},
+      new String[]{"katal", "kat"},
+      new String[]{"lumen", "lm"},
+      new String[]{"lux", "lx"},
+      new String[]{"newton", "N"},
+      new String[]{"ohm", ""+(char)0x2126},
+      new String[]{"pascal", "Pa"},
+      new String[]{"radian", "rad"},
+      new String[]{"siemens", "S"},
+      new String[]{"sievert", "Sv"},
+      new String[]{"steradian", "sr"},
+      new String[]{"tesla", "T"},
       new String[]{"volt", "V"},
+      new String[]{"watt", "W"},
+      new String[]{"weber", "Wb"},
+   };
+
+   private static final String[][] NON_SI_UNITS = new String[][]
+   {
+      new String[]{"day", "d"},
+      new String[]{"degree", ""+(char)+0x00B0},
+      new String[]{"hectare", "ha"},
+      new String[]{"hour", "h"},
+      new String[]{"litre", "l"},
+      new String[]{"liter", "L"},
+      new String[]{"minute", "min"},
+      new String[]{"arcminute", ""+(char)0x2032},
+      new String[]{"arcsecond", ""+(char)0x2033},
+      new String[]{"tonne", "t"},
+      new String[]{"angstrom", ""+(char)0x212B},
+      new String[]{"bar", "bar"},
+      new String[]{"barn", "b"},
+      new String[]{"bel", "B"},
+      new String[]{"decibel", "dB"},
+      new String[]{"knot", "kn"},
+      new String[]{"mmHg", "mmHg"},
+      new String[]{"nauticalmile", "M"},
+      new String[]{"neper", "Np"},
+      new String[]{"astronomicalunit", "ua"},
+      new String[]{"atomicmassunit", "u"},
+      new String[]{"dalton", "Da"},
+      new String[]{"electronvolt", "eV"},
+      new String[]{"bohr", 
+        new String(new int[]{0x1D44E, 0x2080}, 0, 2)},
+      new String[]{"clight",
+        new String(new int[]{0x1D450, 0x2080}, 0, 2)},
+      new String[]{"electronmass", 
+        new String(new int[]{0x1D45A, 0x2091}, 0, 2)},
+      new String[]{"elementarycharge", 
+        new String(new int[]{0x1D452}, 0, 1)},
+      new String[]{"hartree", 
+        new String(new int[]{0x1D438, 0x2095}, 0, 2)},
+      new String[]{"planckbar", ""+(char)0x210F},
+   };
+
+   private static final String[][] ABBREVIATED_UNITS = new String[][]
+   {
+      new String[]{"fg", "fg"},
+      new String[]{"pg", "pg"},
+      new String[]{"ng", "ng"},
+      new String[]{"ug", new String(new int[]{0x00B5, 'g'}, 0, 2)},
+      new String[]{"mg", "mg"},
+      new String[]{"g", "g"},
+      new String[]{"kg", "kg"},
+      new String[]{"amu", "u"},
+      new String[]{"pm", "pm"},
+      new String[]{"nm", "nm"},
+      new String[]{"um", new String(new int[]{0x00B5, 'm'}, 0, 2)},
+      new String[]{"mm", "mm"},
+      new String[]{"cm", "cm"},
+      new String[]{"dm", "dm"},
+      new String[]{"m", "m"},
+      new String[]{"km", "km"},
+      new String[]{"as", "as"},
+      new String[]{"fs", "fs"},
+      new String[]{"ps", "ps"},
+      new String[]{"ns", "ns"},
+      new String[]{"us", new String(new int[]{0x00B5, 's'}, 0, 2)},
+      new String[]{"ms", "ms"},
+      new String[]{"s", "s"},
+      new String[]{"fmol", "fmol"},
+      new String[]{"pmol", "pmol"},
+      new String[]{"nmol", "nmol"},
+      new String[]{"umol", new String(new int[]{0x00B5, 'm', 'o', 'l'}, 0, 4)},
+      new String[]{"mmol", "mmol"},
+      new String[]{"mol", "mol"},
+      new String[]{"kmol", "kmol"},
+      new String[]{"pA", "pA"},
+      new String[]{"nA", "nA"},
+      new String[]{"uA", new String(new int[]{0x00B5, 'A'}, 0, 2)},
+      new String[]{"A", "A"},
+      new String[]{"kA", "kA"},
+      new String[]{"ul", new String(new int[]{0x00B5, 'l'}, 0, 2)},
+      new String[]{"ml", "ml"},
+      new String[]{"l", "l"},
+      new String[]{"hl", "hl"},
+      new String[]{"uL", new String(new int[]{0x00B5, 'L'}, 0, 2)},
+      new String[]{"mL", "mL"},
+      new String[]{"L", "L"},
+      new String[]{"hL", "hL"},
+      new String[]{"mHz", "mHz"},
+      new String[]{"Hz", "Hz"},
+      new String[]{"kHz", "kHz"},
+      new String[]{"MHz", "MHz"},
+      new String[]{"GHz", "GHz"},
+      new String[]{"THz", "THz"},
+      new String[]{"mN", "mN"},
+      new String[]{"N", "N"},
+      new String[]{"kN", "kN"},
+      new String[]{"MN", "MN"},
+      new String[]{"Pa", "Pa"},
+      new String[]{"kPa", "kPa"},
+      new String[]{"MPa", "MPa"},
+      new String[]{"GPa", "GPa"},
+      new String[]{"mohm", new String(new int[]{'m', 0x2126}, 0, 2)},
+      new String[]{"kohm", new String(new int[]{'k', 0x2126}, 0, 2)},
+      new String[]{"Mohm", new String(new int[]{'M', 0x2126}, 0, 2)},
+      new String[]{"pV", "pV"},
+      new String[]{"nV", "nV"},
+      new String[]{"uV", new String(new int[]{0x00B5, 'V'}, 0, 2)},
+      new String[]{"mV", "mV"},
+      new String[]{"V", "V"},
+      new String[]{"kV", "kV"},
+      new String[]{"W", "W"},
+      new String[]{"uW", new String(new int[]{0x00B5, 'W'}, 0, 2)},
+      new String[]{"mW", "mW"},
+      new String[]{"kW", "kW"},
+      new String[]{"MW", "MW"},
+      new String[]{"GW", "GW"},
+      new String[]{"J", "J"},
+      new String[]{"kJ", "kJ"},
+      new String[]{"eV", "eV"},
+      new String[]{"meV", "meV"},
+      new String[]{"keV", "keV"},
+      new String[]{"MeV", "MeV"},
+      new String[]{"GeV", "GeV"},
+      new String[]{"TeV", "TeV"},
+      new String[]{"kWh", "kWh"},
+      new String[]{"F", "F"},
+      new String[]{"fF", "fF"},
+      new String[]{"pF", "pF"},
+      new String[]{"K", ""+(char)0x212A},
+      new String[]{"dB", "dB"},
    };
 
    private static final String[][] PREFIXES = new String[][]
@@ -305,6 +503,35 @@ public class SIunitxSty extends LaTeXSty
       new String[]{"centi", "c"},
       new String[]{"kilo", "k"},
       new String[]{"milli", "m"},
+      new String[]{"mega", "M"},
+      new String[]{"giga", "G"},
+      new String[]{"tera", "T"},
+      new String[]{"yocto", "y"},
+      new String[]{"zepto", "z"},
+      new String[]{"atto", "a"},
+      new String[]{"femto", "f"},
+      new String[]{"pico", "p"},
+      new String[]{"nano", "n"},
+      new String[]{"micro", ""+(char)0x00B5},
+      new String[]{"deci", "d"},
+      new String[]{"deca", "da"},
+      new String[]{"hecto", "h"},
+      new String[]{"peta", "P"},
+      new String[]{"exa", "E"},
+      new String[]{"zetta", "Z"},
+      new String[]{"yotta", "Y"},
+   };
+
+   private static final String[][] BINARY_PREFIXES = new String[][]
+   {
+      new String[]{"kibi", "Ki"},
+      new String[]{"mebi", "Mi"},
+      new String[]{"gibi", "Gi"},
+      new String[]{"tebi", "Ti"},
+      new String[]{"pebi", "Pi"},
+      new String[]{"exbi", "Ei"},
+      new String[]{"zebi", "Zi"},
+      new String[]{"yobi", "Yi"},
    };
 
    public static final int UNIT_SEP = Space.MEDIUM_MATHEMATICAL_SPACE;
