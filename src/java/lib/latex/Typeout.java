e95352f43c50aa32f89141708be36adc07e502a3
nlct <n.talbot@uea.ac.uk>
2020-09-23 14:01:51 +0100

    added typeout

---
+++ src/java/lib/latex/Typeout.java
@@ -0,0 +1,77 @@
+/*
+    Copyright (C) 2020 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texparserlib.latex;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import com.dickimawbooks.texparserlib.*;
+
+public class Typeout extends ControlSequence
+{
+   public Typeout()
+   {
+      this("typeout");
+   }
+
+   public Typeout(String name)
+   {
+      super(name);
+   }
+
+   public Object clone()
+   {
+      return new Typeout(getName());
+   }
+
+   public void process(TeXParser parser) throws IOException
+   {
+      TeXObject arg = parser.popNextArg();
+
+      if (arg instanceof Expandable)
+      {
+         TeXObjectList expanded = ((Expandable)arg).expandfully(parser);
+
+         if (expanded != null)
+         {
+            arg = expanded;
+         }
+      }
+
+      parser.getListener().getTeXApp().message(arg.toString(parser));
+   }
+
+   public void process(TeXParser parser, TeXObjectList stack) throws IOException
+   {
+      TeXObject arg = stack.popArg(parser);
+
+      if (arg instanceof Expandable)
+      {
+         TeXObjectList expanded = ((Expandable)arg).expandfully(parser, stack);
+
+         if (expanded != null)
+         {
+            arg = expanded;
+         }
+      }
+
+      parser.getListener().getTeXApp().message(arg.toString(parser));
+   }
+
+}
