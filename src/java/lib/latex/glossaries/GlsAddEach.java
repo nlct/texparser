da1874091710aff60fc7a8849ff737ca649e4c17
nlct <n.talbot@uea.ac.uk>
2022-08-30 12:18:43 +0100

    added \glsaddeach

---
+++ src/java/lib/latex/glossaries/GlsAddEach.java
@@ -0,0 +1,78 @@
+/*
+    Copyright (C) 2022 Nicola L.C. Talbot
+    www.dickimaw-books.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+package com.dickimawbooks.texparserlib.latex.glossaries;
+
+import java.io.IOException;
+
+import com.dickimawbooks.texparserlib.*;
+import com.dickimawbooks.texparserlib.latex.*;
+
+public class GlsAddEach extends ControlSequence
+{
+   public GlsAddEach()
+   {
+      this("glsaddeach");
+   }
+
+   public GlsAddEach(String name)
+   {
+      super(name);
+   }
+
+   public Object clone()
+   {
+      return new GlsAddEach(getName());
+   }
+
+   @Override
+   public void process(TeXParser parser, TeXObjectList stack)
+     throws IOException
+   {
+      TeXObject optArg = popOptArg(parser, stack);
+      String[] labels = popLabelString(parser, stack).split(",");
+
+      TeXParserListener listener = parser.getListener();
+      TeXObjectList list = listener.createStack();
+
+      ControlSequence cs = listener.getControlSequence("glsadd");
+
+      for (String label : labels)
+      {
+         list.add(cs);
+
+         if (optArg != null)
+         {
+            list.add(listener.getOther('['));
+            list.add(optArg, true);
+            list.add(listener.getOther(']'));
+         }
+
+         list.add(listener.createGroup(label));
+      }
+
+      TeXParserUtils.process(list, parser, stack);
+   }
+
+   @Override
+   public void process(TeXParser parser)
+     throws IOException
+   {
+      process(parser, parser);
+   }
+}
