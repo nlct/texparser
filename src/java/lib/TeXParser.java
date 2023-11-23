d8694aad2a53a8ba65e01652524ed5b6f7bec808
nlct <n.talbot@uea.ac.uk>
2023-11-23 19:20:39 +0000

    updated datatool v3.0 changes

--- src/java/lib/TeXParser.java
+++ src/java/lib/TeXParser.java
@@ -4322,6 +4322,6 @@ public class TeXParser extends TeXObjectList
    public static final int DEBUG_CATCODE = 16384;
    public static final int DEBUG_READ = 32768;
 
-   public static final String VERSION = "0.9.8b.20231013";
-   public static final String VERSION_DATE = "2023-10-13";
+   public static final String VERSION = "0.9.8b.20231123";
+   public static final String VERSION_DATE = "2023-11-23";
 }
