6f3a07ce078ae7fb32ddb25191923647992550db
nlct <n.talbot@uea.ac.uk>
2024-08-20 16:51:15 +0100

    added comments

--- src/java/lib/TeXApp.java
+++ src/java/lib/TeXApp.java
@@ -22,63 +22,250 @@ import java.nio.charset.Charset;
 import java.io.IOException;
 import java.io.File;
 
+/**
+ * Application interface for TeXParser.
+ * An application that uses an instance of TeXParser to parse
+ * files needs to supply a TeXParserListener (which determines how
+ * to interpret the commands and active characters found in the
+ * files) and that in turn requires a TeXApp, which deals with 
+ * messages and I/O. It's the application's TeXApp that determines
+ * what level of file access is permitted (on top of the JVM's
+ * file access permissions) and whether or not external processes
+ * can be spawned.
+ */
+
 public interface TeXApp
 {
-   // Gets output of kpsewhich arg
+   /**
+    * Gets the output of kpsewhich arg. This method may spawn a
+    * sub-process to run kpsewhich with the required argument
+    * (in which case it may throw an InterruptedException
+    * if the process is interrupted)
+    * or may provide its own implementation of kpsewhich
+    * or may simply return null. This method is used for
+    * finding a file on TeX's search (in which the case, the
+    * argument is the filename) or for querying TeX variables,
+    * such as <code>openin_any</code> (in which case,
+    * the argument should be in the form
+    * <code>-var-value=<em>var-name</em></code>).
+    *
+    * If you choose to run kpsewhich, you might want to consider
+    * caching the result.
+    *
+    * @param arg the argument to pass to kpsewhich
+    * @return the output of kpsewhich or null if the query failed or
+    * if the action is not supported
+    */
    public String kpsewhich(String arg)
      throws IOException,InterruptedException;
 
-   // Runs epstopdf --output=pdfFile epsFile
-   // This method should check read and write permissions
+   /**
+    * Converts the given EPS file to a PDF file.
+    * For example, this method may create a process
+    * that runs <code>epstopdf --output=pdfFile epsFile</code>
+    * in which case it may throw an InterruptedException
+    * if the process is interrupted.
+    *
+    * This method should check read and write permissions and may do
+    * nothing if the file conversion request should be ignored.
+    *
+    * @param epsFile the source EPS file
+    * @param pdfFile the destination PDF file
+    */
    public void epstopdf(File epsFile, File pdfFile)
      throws IOException,InterruptedException;
 
-   // Runs wmftoeps -o epsFile wmfFile
-   // This method should check read and write permissions
+   /**
+    * Converts the given WMF file to a PDF file.
+    * For example, this method may create a process
+    * that runs <code>wmftoeps -o epsFile wmfFile</code>
+    * in which case it may throw an InterruptedException
+    * if the process is interrupted.
+    *
+    * This method should check read and write permissions and may do
+    * nothing if the file conversion request should be ignored.
+    */
    public void wmftoeps(File wmfFile, File epsFile)
      throws IOException,InterruptedException;
 
-   // Runs convert [inOptions] inFile [outOptions] outFile
-   // This method should check read and write permissions
+   /**
+    * Converts the given image file.
+    * For example, this method may create a process
+    * that runs <code>magick [inOptions] inFile [outOptions] outFile</code>
+    * in which case it may throw an InterruptedException
+    * if the process is interrupted.
+    *
+    * This method should check read and write permissions and may do
+    * nothing if the file conversion request should be ignored.
+    *
+    * @param inPage the required page number if the source file
+    * contains multiple pages
+    * @param inOptions a list of ImageMagick input options
+    * @param inFile the source image file
+    * @param outOptions a list of ImageMagick output options
+    * @param outFile the destination file
+    */
    public void convertimage(int inPage, String[] inOptions, File inFile,
      String[] outOptions, File outFile)
      throws IOException,InterruptedException;
 
+   /**
+    * Reports to the user that the listener
+    * has substitute some source code. This is typically
+    * when obsolete or deprecated LaTeX code is replaced by newer
+    * commands with LaTeX2LaTeX.
+    *
+    * This method may do nothing if the information isn't deemed
+    * significant.
+    *
+    * @param TeXParser the TeXParser object that's being used to
+    * parse the source files
+    * @param original the original code
+    * @param replacement the replacement code
+    */
    public void substituting(TeXParser parser, String original, String replacement);
 
+   /**
+    * Gets a message identified by a label that may require
+    * parameters. This method should hook into the localisation
+    * commands provided by your application.
+    *
+    * See the file lib/resources/dictionaries/texparserlib-en.xml 
+    * for an example set of messages.
+    */
    public String getMessage(String label, Object... params);
 
+   /**
+    * Requests input from the user. This method may ignore the
+    * request and return null.
+    *
+    * @param message the prompt for the user
+    * @return the response from the user or null
+    */
    public String requestUserInput(String message)
      throws IOException;
 
+   /**
+    * Prints a message. The message may be written to STDOUT or
+    * a GUI element or may be ignored.
+    *
+    * @param text the message
+    */
    public void message(String text);
 
+   /**
+    * Prints a warning. The message may be written to STDERR or
+    * a GUI element.
+    *
+    * @param parser the TeXParser being used to parse the files.
+    * @param message the warning message that needs to be reported
+    */
    public void warning(TeXParser parser, String message);
 
-   public void error(Exception e);
+   /**
+    * Reports that an exception has occurred.
+    *
+    * @param excpt the exception
+    */
+   public void error(Exception excpt);
 
+   /**
+    * Reports the progress for a potentially long-running process.
+    * This method may do nothing.
+    * @param percentage the percentage progress
+    */
    public void progress(int percentage);
 
-   // This method should check read and write permissions
+   /**
+    * Copies a file.
+    * This method should check read and write permissions or may do
+    * nothing, issue a warning or throw an exception if copying isn't supported.
+    *
+    * @param orgFile the source file
+    * @param newFile the destination file
+    */
    public void copyFile(File orgFile, File newFile)
      throws IOException,InterruptedException;
 
+   /**
+    * Determines if read access is allowed for the given file.
+    * @param path a TeXPath representation of the file
+    * @return true if read access is allowed or false otherwise
+    */
    public boolean isReadAccessAllowed(TeXPath path);
+
+   /**
+    * Determines if read access is allowed for the given file.
+    * @param file the file
+    * @return true if read access is allowed or false otherwise
+    */
    public boolean isReadAccessAllowed(File file);
 
+   /**
+    * Determines if write access is allowed for the given file.
+    * @param path a TeXPath representation of the file
+    * @return true if write access is allowed or false otherwise
+    */
    public boolean isWriteAccessAllowed(TeXPath path);
+
+   /**
+    * Determines if write access is allowed for the given file.
+    * @param file the file
+    * @return true if write access is allowed or false otherwise
+    */
    public boolean isWriteAccessAllowed(File file);
 
+   /**
+    * Gets the default encoding. This may simply return
+    * java.nio.charset.Charset.defaultCharset().
+    *
+    * @return the default encoding
+    */
    public Charset getDefaultCharset();
 
+   /**
+    * Gets the application name.
+    */ 
    public String getApplicationName();
 
+   /**
+    * Gets the application version.
+    */ 
    public String getApplicationVersion();
 
+   /**
+    * Localisation message label indicating that a file is about to
+    * be read. Takes one parameter (the filename).
+    */
    public static String MESSAGE_READING = "message.reading";
+
+   /**
+    * Localisation message label indicating that a file is about to
+    * be written. Takes one parameter (the filename).
+    */
    public static String MESSAGE_WRITING = "message.writing";
+
+   /**
+    * Localisation message label indicating that a file 
+    * does not have read access. Takes one parameter (the filename).
+    */
    public static String MESSAGE_NO_READ = "message.no.read";
+
+   /**
+    * Localisation message label indicating that a file 
+    * does not have write access. Takes one parameter (the filename).
+    */
    public static String MESSAGE_NO_WRITE = "message.no.write";
+
+   /**
+    * Localisation message label used to report the
+    * selected encoding. Takes one parameter (the encoding name).
+    */
    public static String MESSAGE_ENCODING = "message.charset";
+
+   /**
+    * Localisation message label indicating that a file 
+    * is being overwritten. Takes one parameter (the filename).
+    */
    public static String WARNING_OVERWRITING = "warning.overwriting";
 }
