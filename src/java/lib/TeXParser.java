33f740dac979c5d390bce21f66491db77b065677
Nicola Talbot <n.talbot@uea.ac.uk>
2025-08-16 16:24:47 +0100

    corrected version

--- src/java/lib/TeXParser.java
+++ src/java/lib/TeXParser.java
@@ -4753,6 +4753,6 @@ public class TeXParser extends TeXObjectList
    public static final int DEBUG_READ = 32768;
    public static final int DEBUG_SETTINGS = 65536;
 
-   public static final String VERSION = "1.5.20250815";
-   public static final String VERSION_DATE = "2025-08-15";
+   public static final String VERSION = "1.6";
+   public static final String VERSION_DATE = "2025-08-16";
 }
