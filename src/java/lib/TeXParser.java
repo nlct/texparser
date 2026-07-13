c7bcd1611168960b6376c1de15cf4ebc2467f49e
Nicola Talbot <n.talbot@uea.ac.uk>
2026-07-13 16:06:17 +0100

    updated version date

--- src/java/lib/TeXParser.java
+++ src/java/lib/TeXParser.java
@@ -5297,6 +5297,6 @@ public class TeXParser extends TeXObjectList
    /** Scoping debugging flag. */
    public static final int DEBUG_SETTINGS = 65536;
 
-   public static final String VERSION = "1.9.20260711";
-   public static final String VERSION_DATE = "2026-07-11";
+   public static final String VERSION = "1.9.20260713";
+   public static final String VERSION_DATE = "2026-07-13";
 }
